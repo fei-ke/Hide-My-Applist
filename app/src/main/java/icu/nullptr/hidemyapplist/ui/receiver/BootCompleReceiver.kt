e086cb8ea2cc85fe3b315a585ec4f75f42e51fcf
fei-ke <jinyang656@gmail.com>
2023-09-20 23:51:59 +0800

    Launch HMA when the device boot completed to listen newly installed package

---
+++ app/src/main/java/icu/nullptr/hidemyapplist/ui/receiver/BootCompleReceiver.kt
@@ -0,0 +1,11 @@
+package icu.nullptr.hidemyapplist.ui.receiver
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class BootCompleReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+
+    }
+}
